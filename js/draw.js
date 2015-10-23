738e7cc37d908cebbfad86bd3305c002c312f10b
Vaclav Novotny <vaclavnovotny@vaclavnovotny.eu>
2015-10-23 16:08:05 +0200

    chapter 6

--- js/draw.js
+++ js/draw.js
@@ -5,7 +5,8 @@ var Draw = {
 	_context: null
 };
 
-Draw.init = function(){
+/* Výroba canvasu a jeho příprava */
+Draw.init = function() {
 	var canvas = document.createElement("canvas");
 
 	this.CELL += this.LINE;
@@ -14,58 +15,106 @@ Draw.init = function(){
 	canvas.width = size;
 	canvas.height = size;
 
-	this._context = canvas.getContext("2d")
+	this._context = canvas.getContext("2d");
 	this._context.lineWidth = this.LINE;
 
 	document.body.appendChild(canvas);
 
 	this.all();
-};
+}
 
-Draw.all = function(){
-	var html = "<table>";
+/* Vykreslit celou hrací plochu */
+Draw.all = function() {
+	this._context.fillStyle = "#fff";
+	var width = this._context.canvas.width;
+	var height = this._context.canvas.height;
 
-	for (var i = 0 ; i < Board.length ; i++) {
-		html += "<tr>";
-		for (var j = 0; j < Board[i].length ; j++) {
-			console.log('writing TD');
-			html += "<td>";
-			html += Draw.atoms(Board[j][i]);
-			html += "</td>";
-		};
-		html += "</tr>";
-	}
-	html += "</table>";
+	this._context.fillRect(0, 0, width, height);
 
-	document.body.innerHTML = html;
+	this._lines();
+	this._cells();
+}
 
-};
+/* Vykreslit mřížku */
+Draw._lines = function() {
+	this._context.beginPath();
 
-Draw.atoms = function(count){
-	var result = "";
-
-	for (var i = 0; i < count; i++) {
-		result += "O";
-	};
-
-	return result;
-};
-
-Draw.getPosition = function (node){
-	if (node.nodeName!= "TD") {return null;}
-
-	var x = 0;
-	while (node.previousSibling){
-		x++;
-		node = node.previousSibling;
+	for (var i=0; i<Game.SIZE+1; i++) { // svislé
+		var x = this.LINE/2 + i*this.CELL;
+		this._context.moveTo(x, 0);
+		this._context.lineTo(x, this._context.canvas.height);
 	}
 
-	var row = node.parentNode;
-	var y = 0;
-	while (row.previousSibling){
-		y++;
-		row = row.previousSibling;
+	for (var i=0; i<Game.SIZE+1; i++) { // vodorovné
+		var y = this.LINE/2 + i*this.CELL;
+		this._context.moveTo(0, y);
+		this._context.lineTo(this._context.canvas.width, y);
 	}
 
-	return [x , y];
-};
+	this._context.stroke();
+}
+
+/* Vykreslit buňky s atomy */
+Draw._cells = function() {
+	for (var i=0; i<Game.SIZE; i++) {
+		for (var j=0; j<Game.SIZE; j++) {
+			this._cell(i, j, Board[i][j]);
+		}
+	}
+}
+
+/* Vykreslit jednu buňku */
+Draw._cell = function(x, y, count) {
+	x *= this.CELL;
+	y *= this.CELL;
+	switch (count) {
+		case 1:
+			this._atom(x + this.CELL/2, y + this.CELL/2);
+		break;
+
+		case 2:
+			this._atom(x + this.CELL/4, y + this.CELL/4);
+			this._atom(x + this.CELL*3/4, y + this.CELL*3/4);
+		break;
+
+		case 3:
+			this._atom(x + this.CELL/2, y + this.CELL/2);
+			this._atom(x + this.CELL/4, y + this.CELL/4);
+			this._atom(x + this.CELL*3/4, y + this.CELL*3/4);
+		break;
+
+		case 4:
+			this._atom(x + this.CELL/4,   y + this.CELL/4);
+			this._atom(x + this.CELL*3/4, y + this.CELL*3/4);
+			this._atom(x + this.CELL/4,   y + this.CELL*3/4);
+			this._atom(x + this.CELL*3/4, y + this.CELL/4);
+		break;
+	}
+}
+
+/* Vykreslit jeden atom */
+Draw._atom = function(x, y) {
+	this._context.beginPath();
+
+	this._context.moveTo(x+this.ATOM, y);
+	this._context.arc(x, y, this.ATOM, 0, 2*Math.PI, false);
+
+	this._context.fillStyle = "blue";
+	this._context.fill();
+	this._context.stroke();
+}
+
+/* Převod pozice kurzoru na souřadnice buňky */
+Draw.getPosition = function(cursorX, cursorY) {
+	var rectangle = this._context.canvas.getBoundingClientRect();
+
+	cursorX -= rectangle.left;
+	cursorY -= rectangle.top;
+
+	if (cursorX < 0 || cursorX > rectangle.width) { return null; }
+	if (cursorY < 0 || cursorY > rectangle.height) { return null; }
+
+	var cellX = Math.floor(cursorX / this.CELL);
+	var cellY = Math.floor(cursorY / this.CELL);
+	return [cellX, cellY];
+}
--- js/player.js
+++ js/player.js
@@ -2,18 +2,14 @@ var Player = {};
 
 Player.listen = function() {
 	document.body.addEventListener("click", Player.click);
-};
+}
 
 Player.click = function(e) {
-	var position = Draw.getPosition(e.target);
-	if (!position) {
-		return
-	};
+	var position = Draw.getPosition(e.clientX, e.clientY);
+	if (!position) { return; }
 
 	var x = position[0];
 	var y = position[1];
-
 	Board[x][y]++;
 	Draw.all();
-};
-
+}
